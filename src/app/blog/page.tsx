3788999925e84738cb54818408b42c4973335b81
sirawan1003 <sirawan06102546@gmail.com>
2024-06-22 15:38:28 +0700

    change pink to green

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,6 @@
 export default function Blog() {
     return (
-        <div className="text-pink-400 text-9xl text-center content-center bg-black rounded-lg my-10 mx-10 pb-3">
+        <div className="text-green-400 text-9xl text-center content-center bg-black rounded-lg my-10 mx-10 pb-3">
             Hello WU
         </div>
     )
