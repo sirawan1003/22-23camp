af53bef2cc52cac9f76300349448ca7a0f49f89c
sirawan1003 <sirawan06102546@gmail.com>
2024-06-22 15:35:36 +0700

    init

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return (
+        <div className="text-pink-400 text-9xl text-center content-center bg-black rounded-lg my-10 mx-10 pb-3">
+            Hello WU
+        </div>
+    )
+}
